344709cecdf1f856759a39395eec463527ea4939
AliveRg <renatgorodov49280@gmail.com>
2024-05-17 12:36:24 +0300

    LAST TEST COMM

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ import { RouterLink, RouterView } from 'vue-router'
           <div class="text-sm font-normal flex items-center gap-5">
             <RouterLink to="/">Главная</RouterLink>
 
-            <RouterLink to="/catalog">Каталог</RouterLink>
+            <RouterLink :to="{ name: 'catalog' }">Каталог</RouterLink>
             <RouterLink to="/about">О Нас</RouterLink>
           </div>
           <img src="@/assets/Logo.svg" alt="" />
@@ -88,16 +88,30 @@ import { RouterLink, RouterView } from 'vue-router'
         </div>
       </div>
       <div
-        class="bg-black/70 relative z-30 md:bg-black md:w-[40%] px-[15px] py-[10px] md:px-[80px] md:py-[40px] mx-auto text-center flex flex-col items-center justify-start"
+        class="bg-black/70 relative z-30 md:bg-black md:w-[40%] px-[15px] py-[10px] md:px-[80px] md:py-[40px] mx-auto text-center"
       >
-        <p class="text-[29px] mb-[22px]">Ты старше 21 года?</p>
-        <p class="text-lg mb-[31px]">Просим давать корректную информацию</p>
-        <input
-          ref="inputField"
-          v-model="inputText"
-          class="mb-[35px] bg-transparent border border-white px-[20px] py-[10px]"
-          type="text"
-        />
+        <Transition mode="out-in">
+          <div v-if="acticute" class="flex flex-col items-center justify-start">
+            <p class="text-[29px] mb-[22px]">Ты старше 21 года?</p>
+            <p class="text-lg mb-[31px]">Просим давать корректную информацию</p>
+            <input
+              ref="inputField"
+              v-model="inputText"
+              class="mb-[35px] bg-transparent border border-white px-[20px] py-[10px]"
+              type="text"
+            />
+            <div
+              v-if="inputText"
+              class="px-[58px] py-[13px] bg-white text-black"
+              @click="acticute = false"
+            >
+              Сохранить
+            </div>
+          </div>
+          <div v-else class="flex flex-col items-center justify-start">
+            <p class="text-[29px] mb-[22px]">Спасибо, что помогаете улучшить проект</p>
+          </div>
+        </Transition>
       </div>
     </section>
 
@@ -110,14 +124,91 @@ import { RouterLink, RouterView } from 'vue-router'
         frameborder="0"
       ></iframe>
     </section>
-    <hr class="mx-[81px] mb-[100px] mt-[100px] font-sans" />
+    <hr class="mx-[21px] mb-[10px] mt-[10px] font-sans" />
   </div>
+  <footer class="text-gray-600 body-font">
+    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <a
+        class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+      >
+        <img src="@/assets/BlackLogo.svg" alt="" />
+      </a>
+      <p
+        class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
+      >
+        © 2024 RITM Company —
+        <a href="" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank"
+          >@ritmcreate</a
+        >
+      </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a class="text-gray-500">
+          <svg
+            fill="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg
+            fill="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+            ></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a class="ml-3 text-gray-500">
+          <svg
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="0"
+            class="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="none"
+              d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+            ></path>
+            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          </svg>
+        </a>
+      </span>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
 export default {
   data() {
     return {
+      acticute: true,
       banner: true,
       stick: false,
       inputText: ''
@@ -169,6 +260,15 @@ export default {
 </script>
 
 <style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 .sticky-menu {
   top: 60px; /* Начальное положение ниже */
   transition: all 0.5s;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,156 @@
+<script lang="ts" setup>
+import { defineComponent } from 'vue'
+</script>
 <template>
-  <div></div>
+  <div>
+    <div class="w-full bg-[#996B60]">
+      <div class="">
+        <div
+          class="menu sticky-menu top-0 z-20 flex w-full items-center justify-between bg-transparent px-[60px] py-[24px] uppercase leading-[18px] tracking-wide text-white transition-all"
+        >
+          <div class="flex items-center gap-5 text-sm font-normal">
+            <RouterLink to="/">Главная</RouterLink>
+
+            <RouterLink to="/catalog">Каталог</RouterLink>
+            <RouterLink to="/about">О Нас</RouterLink>
+          </div>
+          <img src="@/assets/Logo.svg" alt="" />
+          <div class="flex w-1/4 items-center gap-5 text-sm font-normal">
+            <!-- <p>Главная</p>
+            <p>Главная</p>
+            <p>Главная</p> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="section-1 container mx-auto my-[85px] flex justify-between items-center">
+      <div class="grid grid-cols-2 w-1/2 gap-[30px]">
+        <div class="grid grid-cols-1 gap-[30px] mt-[50px]">
+          <div
+            class="panel w-full h-full rounded-2xl bg-[#EEEEEE] font-bold text-[20px] text-[#222831] p-[20px]"
+          >
+            <p class="mb-3">Страховка</p>
+            <p class="font-semibold text-[16px]">
+              Не волнуйтесь о поломках, все инструменты застрахованы, поэтому при неудачах вы
+              защищены от лишних трат.
+            </p>
+          </div>
+          <div
+            class="panel w-full h-full rounded-2xl bg-[#00ADB5] font-bold text-[20px] text-[#EEEEEE] p-[20px]"
+          >
+            <p class="mb-3">Удобство</p>
+            <p class="font-semibold text-[16px]">
+              Наши шоурумы расположены по всей России, вы сможе лично убедиться в качестве или
+              заказать доставку.
+            </p>
+          </div>
+        </div>
+        <div class="grid grid-cols-1 gap-[30px] mb-[50px]">
+          <div
+            class="panel w-full h-full rounded-2xl bg-[#393E46] font-bold text-[20px] text-[#EEEEEE] p-[20px]"
+          >
+            <p class="mb-3">Доступность</p>
+            <p class="font-semibold text-[16px]">
+              Мы предоставляем возможность экономить, как крупным студиям, так и небольшим группам,
+              предлагая аренду по низкой цене!
+            </p>
+          </div>
+          <div
+            class="panel w-full h-full rounded-2xl bg-[#222831] font-bold text-[20px] text-[#00ADB5] p-[20px]"
+          >
+            <p class="mb-3">Выбор</p>
+            <p class="font-semibold text-[16px]">
+              В нашем каталоге представлен большой выбор различных инструмантов, которые доступны
+              каждому из вас.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="masterimg w-1/2">
+        <img class="transformDD orig w-full" src="@/assets/bgWallpaper.png" alt="" />
+      </div>
+    </section>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto flex flex-wrap">
+        <div class="flex w-full mb-20 flex-wrap">
+          <h1
+            class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4"
+          >
+            Чем мы помогаем
+          </h1>
+          <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">
+            Мы предоставляем оборудование и инструманты на прокат. Многие мазыканты и артисты
+            предпочитают большим тратам на покупку, аренду на время концерта или тура.
+          </p>
+        </div>
+        <div class="flex flex-wrap md:-m-2 -m-1">
+          <div class="flex flex-wrap w-full md:w-1/2">
+            <div class="md:p-2 p-1 w-full sm:w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://i.pinimg.com/564x/29/c5/a2/29c5a2fad027b0b762267f16292e12d8.jpg"
+              />
+            </div>
+            <div class="md:p-2 p-1 w-full sm:w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://i.pinimg.com/564x/1c/3c/7e/1c3c7eb7d49bf4d668f702ae602087c5.jpg"
+              />
+            </div>
+            <div class="md:p-2 p-1 w-full">
+              <img
+                alt="gallery"
+                class="w-full h-full object-cover object-center block"
+                src="https://i.pinimg.com/564x/4b/67/79/4b67794cfcadd6fc3571ea3bcbe868c6.jpg"
+              />
+            </div>
+          </div>
+          <div class="flex flex-wrap w-full md:w-1/2">
+            <div class="md:p-2 p-1 w-full">
+              <img
+                alt="gallery"
+                class="w-full h-full object-cover object-center block"
+                src="https://i.pinimg.com/564x/03/1d/94/031d940dec1448a484e61b309dbd60a0.jpg"
+              />
+            </div>
+            <div class="md:p-2 p-1 w-full sm:w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://i.pinimg.com/564x/a6/75/98/a675981d5a332c3680df351d3d3bbac2.jpg"
+              />
+            </div>
+            <div class="md:p-2 p-1 w-full sm:w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://i.pinimg.com/564x/ef/b8/4e/efb84eba2a6a6111e0479f619055f468.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts">
+export default defineComponent({
+  data() {
+    return {}
+  }
+})
+</script>
 
-<style></style>
+<style scoped>
+.masterimg {
+  perspective: 1000px;
+}
+.transformDD {
+  transform: rotateX(1deg) rotateY(-20deg);
+  transform-style: preserve-3d;
+}
+</style>
